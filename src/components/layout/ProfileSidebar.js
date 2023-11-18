19fd2c286f3d58227a84041f992202ee97d8202a
Alireza-Khalafpour <alirezakhalafpour98@gmail.com>
2023-11-18 22:00:12 +0330

    responsive profile page

--- src/components/layout/ProfileSidebar.js
+++ src/components/layout/ProfileSidebar.js
@@ -1,5 +1,6 @@
 
 import { Face6, FavoriteBorder, HomeMax, NotificationsNone, Settings } from "@mui/icons-material";
+import { Badge } from "@mui/joy";
 import { Divider, Drawer, List, ListItem } from "@mui/material";
 import Link from "next/link";
 
@@ -10,30 +11,32 @@ const ProfileSidebar = ({children}) => {
             <div className="w-1/6" >
                 <List className="border-l-2 border-paszamine2 h-[100vh] font-extrabold">
 
-                    <h1 className="w-full text-center py-12 text-2xl" > پرفایل </h1>
+                    <h1 className="w-full text-center py-12 md:text-2xl text-lg" > پروفایل </h1>
 
                     <ListItem disablePadding focusRipple className="mb-6" >
                     <Link href="/profile" className='  w-full shadow-none border-l-4 hover:border-khas hover:bg-paszamine2 text-paszamine3 hover:text-black transition-all duration-200 flex justify-start mr-4 cursor-pointer p-4  rounded-md gap-2' >
                         <Face6 />
-                        <span> اطلاعات کاربر </span>
+                        <span className="md:block hidden" > اطلاعات کاربر </span>
                     </Link>
                     </ListItem>
                     <ListItem disablePadding focusRipple className="mb-6" >
                     <Link href="/profile" className='  w-full shadow-none border-l-4 hover:border-khas hover:bg-paszamine2 text-paszamine3 transition-all duration-200 flex justify-start mr-4 cursor-pointer p-4 rounded-md gap-2' >
                         <FavoriteBorder />
-                        <span> لیست علاقه مندی ها </span>
+                        <span className="md:block hidden" > لیست علاقه مندی ها </span>
                     </Link>
                     </ListItem>
                     <ListItem disablePadding focusRipple className="mb-6" >
                     <Link href="/profile" className='  w-full shadow-none border-l-4 hover:border-khas hover:bg-paszamine2 text-paszamine3 transition-all duration-200 flex justify-start mr-4 cursor-pointer p-4 rounded-md gap-2' >
                         <Settings />
-                        <span> تنظیمات </span>
+                        <span className="md:block hidden" > تنظیمات </span>
                     </Link>
                     </ListItem>
                     <ListItem disablePadding focusRipple className="mb-6" >
                     <Link href="/profile" className='  w-full shadow-none border-l-4 hover:border-khas hover:bg-paszamine2 text-paszamine3 transition-all duration-200 flex justify-start mr-4 cursor-pointer p-4 rounded-md gap-2' >
-                        <NotificationsNone />
-                        <span> اعلان ها </span>
+                        <Badge color="warning" >
+                            <NotificationsNone />
+                        </Badge>
+                        <span className="md:block hidden" > اعلان ها </span>
                     </Link>
                     </ListItem>
 
--- src/components/templates/ProfileMainPage.js
+++ src/components/templates/ProfileMainPage.js
@@ -7,8 +7,8 @@ import shazs from "../../../public/images/Sign up-amico.svg"
 
 const ProfileMainPage = () => {
     return (
-        <div className="flex flex-col gap-14 w-[80%] p-12">
-            <div className="flex flex-row justify-around items-center" >
+        <div className="flex flex-col gap-14 md:w-[80%] w-full p-12">
+            <div className="flex md:flex-row flex-col justify-around items-center gap-6" >
 
                 <Badge
                     anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
@@ -20,53 +20,53 @@ const ProfileMainPage = () => {
                     />
                     }
                 >
-                    <Avatar alt="Aravis Howard" src={shazs} size="lg" className="w-64 h-64 border-2 border-paszamine2" />
+                    <Avatar alt="Aravis Howard" src={shazs} size="lg" className="md:w-64 w-40 md:h-64 h-40 border-2 border-paszamine2" />
                 </Badge>
 
                 <div className="gap-3 flex flex-col">
-                    <h2 className="text-2xl font-bold"> آرمین زارعی </h2>
+                    <h2 className="text-2xl font-bold"> 2Afm آرمین زارعی </h2>
                     <p> <Email className="text-khas"/> www.email@gmail.com </p>
                 </div>
 
             </div>
 
-            <div className="flex flex-row justify-around items-center" >
+            <div className="flex md:flex-row flex-col justify-around items-center" >
 
                 <FormControl className="w-full">
                     <FormLabel> نام </FormLabel>
-                    <Input className="w-[70%] shadow-lg " endDecorator={<Dns/>} size="lg" placeholder=" نام " />
+                    <Input className="md:w-[70%] w-full shadow-lg " endDecorator={<Dns/>} size="lg" placeholder=" نام " />
                     {/* <FormHelperText>This is a helper text.</FormHelperText> */}
                 </FormControl>
 
                 <FormControl className="w-full" >
                     <FormLabel> نام خانوادگی </FormLabel>
-                    <Input className="w-[70%] shadow-lg " size="lg" endDecorator={<Dns/>} placeholder="  نام خانوادگی" />
+                    <Input className="md:w-[70%] w-full shadow-lg " size="lg" endDecorator={<Dns/>} placeholder="  نام خانوادگی" />
                     {/* <FormHelperText>This is a helper text.</FormHelperText> */}
                 </FormControl>
 
             </div>
 
-            <div className="flex flex-row justify-around items-center" >
+            <div className="flex md:flex-row flex-col justify-around items-center" >
 
                 <FormControl className="w-full" >
                     <FormLabel> شماره همراه </FormLabel>
-                    <Input className="w-[70%] shadow-lg " endDecorator={<Smartphone/>} size="lg" placeholder="   شماره همراه  " />
+                    <Input className="md:w-[70%] w-full shadow-lg " endDecorator={<Smartphone/>} size="lg" placeholder="   شماره همراه  " />
                     {/* <FormHelperText>This is a helper text.</FormHelperText> */}
                 </FormControl>
 
                 <FormControl className="w-full">
                     <FormLabel> ایمیل </FormLabel>
-                    <Input className="w-[70%] shadow-lg " endDecorator={<Email/>} size="lg" placeholder=" ایمیل " />
+                    <Input className="md:w-[70%] w-full shadow-lg " endDecorator={<Email/>} size="lg" placeholder=" ایمیل " />
                     {/* <FormHelperText>This is a helper text.</FormHelperText> */}
                 </FormControl>
 
             </div>
 
-            <div className="flex flex-row justify-center items-center" >
+            <div className="flex md:flex-row flex-col justify-center items-center" >
 
                 <FormControl className="w-full" >
                     <FormLabel> آدرس و کد پستی </FormLabel>
-                    <Textarea minRows={3} className="w-[85%] text-center shadow-lg " placeholder="   آدرس و کد پستی  " />
+                    <Textarea minRows={3} className="md:w-[85%] w-full text-center shadow-lg " placeholder="   آدرس و کد پستی  " />
                     {/* <FormHelperText>This is a helper text.</FormHelperText> */}
                 </FormControl>
 
